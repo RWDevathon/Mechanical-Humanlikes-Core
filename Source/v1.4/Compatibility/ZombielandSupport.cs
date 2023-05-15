9cd9c9c43d677187d7d947d06be8b4a77e21b4de
Killathon <huelva21@gmail.com>
2023-05-15 16:51:29 -0700

    Added custom Zombieland support

---
+++ Source/v1.4/Compatibility/ZombielandSupport.cs
@@ -0,0 +1,17 @@
+﻿using MechHumanlikes;
+using Verse;
+
+public class ZombielandSupport
+{
+    // Mechanical pawns can not be zombies.
+    public static bool CanBecomeZombie(Pawn pawn)
+    {
+        return !MHC_Utils.IsConsideredMechanical(pawn);
+    }
+
+    // Mechanical pawns do not attract the attention of zombies.
+    public static bool AttractsZombies(Pawn pawn)
+    {
+        return !MHC_Utils.IsConsideredMechanical(pawn);
+    }
+}
